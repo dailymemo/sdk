2ace416857a8f52fe5a34ee9813d768c854d849f
Minghe Huang <h.minghe@gmail.com>
2024-03-29 11:26:06 +0800

    clean up

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 100,
-  singleQuote: true,
-  semi: false,
-  trailingComma: 'all',
-  jsxBracketSameLine: false,
-  bracketSpacing: true,
-  arrowParens: 'always',
-}
